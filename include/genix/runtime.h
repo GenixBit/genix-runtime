59389ffc543880dd8967a694bc3e954af172103a
GenixBit Labs Private Limited <manojnitbhu2@gmail.com>
2026-08-31 01:15:56 +0530

    Add Option Result ABI carrier types

--- include/genix/runtime.h
+++ include/genix/runtime.h
@@ -11,6 +11,16 @@ extern "C" {
 
 #define GENIX_RUNTIME_ABI_VERSION 1
 
+typedef struct { bool has_value; int64_t value; } GbOptionInt;
+typedef struct { bool has_value; double value; } GbOptionFloat;
+typedef struct { bool has_value; bool value; } GbOptionBool;
+typedef struct { bool has_value; const char* value; } GbOptionString;
+
+typedef struct { bool ok; int64_t value; const char* error; } GbResultInt;
+typedef struct { bool ok; double value; const char* error; } GbResultFloat;
+typedef struct { bool ok; bool value; const char* error; } GbResultBool;
+typedef struct { bool ok; const char* value; const char* error; } GbResultString;
+
 void gb_runtime_init(void);
 void gb_runtime_shutdown(void);
 
@@ -28,8 +38,11 @@ void gb_print_string(const char* value);
 /* Host-service APIs used by compiler-recognized standard-library intrinsics. */
 char* gb_input(const char* prompt);
 char* gb_env_get(const char* name);
+GbOptionString gb_env_get_option(const char* name);
 char* gb_fs_read_text(const char* path);
+GbResultString gb_fs_try_read_text(const char* path);
 void gb_fs_write_text(const char* path, const char* text);
+GbResultBool gb_fs_try_write_text(const char* path, const char* text);
 _Noreturn void gb_process_exit(int64_t code);
 
 #ifdef __cplusplus
